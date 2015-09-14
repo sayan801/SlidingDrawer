16dd77ef020637b47927f8553a71de0423391b5c
amiyo-cribello <amiyo.biswas@mycuratio.com>
2015-09-14 12:16:40 +0530

    Added Static Listview For testing Purpose #https://github.com/sayan801/SlidingDrawer/issues/9

--- app/src/main/java/technicise/com/demoslidingdrawerapp/DemoActivity.java
+++ app/src/main/java/technicise/com/demoslidingdrawerapp/DemoActivity.java
@@ -4,8 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class DemoActivity extends AppCompatActivity {
 
@@ -16,10 +20,58 @@ public class DemoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_demo);
 
 
-        FrameLayout frameLayoutObj=(FrameLayout)findViewById(R.id.drawer_bar);
-        TextView tv2 = new TextView(this);
-        tv2.setText("Technicise");
-        frameLayoutObj.addView(tv2);
+        FrameLayout frameLayoutObj=(FrameLayout)findViewById(R.id.background_view);
+
+        final ListView listViewObj = new ListView(this);
+
+        // Defined Array values to show in ListView
+        String[] values = new String[] { "Android List View",
+                "Adapter implementation",
+                "Simple List View In Android",
+                "Create List View Android",
+                "Android Example",
+                "List View Source Code",
+                "List View Array Adapter",
+                "Android Example List View"
+        };
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+
+        // Assign adapter to ListView
+        listViewObj.setAdapter(adapter);
+
+        // ListView Item Click Listener
+        listViewObj.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // ListView Clicked item index
+                int itemPosition     = position;
+
+                // ListView Clicked item value
+                String  itemValue    = (String) listViewObj.getItemAtPosition(position);
+
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                        "Position :" + itemPosition + "  ListItem : " + itemValue, Toast.LENGTH_LONG)
+                        .show();
+
+            }
+
+        });
+
+
+
+        frameLayoutObj.addView(listViewObj);
 
 
     }
